8c8ad76fa5a60b5b06ee96f41472b19e44a73889
chronoswap <dev.chronoswap@gmail.com>
2023-06-02 20:16:59 +0200

    Changed requirements versions

--- bpAdmin/__init__.py
+++ bpAdmin/__init__.py
@@ -4,6 +4,7 @@ from flask import Blueprint, redirect, url_for, current_app, request, flash, ren
 from flask_login import login_required, current_user
 
 from utils.user import setPlayerPermission, getUserOnly
+from utils.package import getAllPackages
 from utils.decorators import only_left_hand, only_collaborator, only_admin
 
 adminBP = Blueprint('adminBluePrint', __name__)
@@ -23,6 +24,7 @@ def changePlayerPermissionsEndPoint(us):
     return render_template(
         'permissions.html',
         usr=usr,
+        packages=getAllPackages(),
         permissions=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
 
 
--- bpGeneric/__init__.py
+++ bpGeneric/__init__.py
@@ -9,11 +9,10 @@ genericBP = Blueprint('genericBluePrint', __name__)
 
 @genericBP.route("/", methods={"GET", "POST"})
 def generalEndPoint():
-    packages = getAllPackages()
     return render_template(
         'general.html',
         title=current_app.config['WEB_NAME'] + " - " + "General",
-        packages=packages,
+        packages=getAllPackages(),
         user=current_user if not current_user.is_anonymous else None
     )
 
@@ -23,5 +22,16 @@ def aboutEndPoint():
     return render_template(
         'about.html',
         title=current_app.config['WEB_NAME'] + " - " + "About",
+        packages=getAllPackages(),
+        user=current_user if not current_user.is_anonymous else None
+    )
+
+
+@genericBP.route("/contact", methods={"GET", "POST"})
+def contactEndPoint():
+    return render_template(
+        'contact.html',
+        title=current_app.config['WEB_NAME'] + " - " + "Contact",
+        packages=getAllPackages(),
         user=current_user if not current_user.is_anonymous else None
     )
